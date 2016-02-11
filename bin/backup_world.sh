4ebbd16b005d1586517942b24cc1ef5fcda4c950
Corey Goldberg <cgoldberg@gmail.com>
2016-02-11 07:07:26 -0500

    Update backup_world.sh

--- bin/backup_world.sh
+++ bin/backup_world.sh
@@ -1,17 +1,21 @@
-#!/bin/sh
-
 set -e
 
+echo "\nrunning master backup job:"
+echo "----------------------------------------------------------------"
 echo "\ndeleting Dropbox cache..."
 rm -rf ~/Dropbox/.dropbox.cache/*
+echo "----------------------------------------------------------------"
 echo "\nsyncing local Dropbox to Bytez NAS..."
-rsync -a --delete --human-readable --progress --info=progress2 --safe-links ~/Dropbox/ /mnt/bytez/Dropbox/
+rsync -a --delete --human-readable --info=progress2 --safe-links ~/Dropbox/ /mnt/bytez/Dropbox/
+echo "----------------------------------------------------------------"
 echo "\nsyncing Bytez NAS to local WD-Green HDD..."
 rsync -a --delete --human-readable --progress --info=progress2 --safe-links /mnt/bytez/ /mnt/wd-green/
+echo "----------------------------------------------------------------"
 echo "\nflushing local file system buffers..."
 sync
-echo "\neverything synced!\n"
+echo "----------------------------------------------------------------\n"
 df -hT /mnt/wd-green
-echo
+echo "----------------------------------------------------------------\n"
 df -hT /mnt/bytez
-echo "\ndone"
+echo "----------------------------------------------------------------\n"
+echo "done"
