30ae7f842cfe3408f3be9f3ec8ab3da918ee72d8
Corey Goldberg <cgoldberg@gmail.com>
2016-02-20 11:48:10 -0500

    refactored installation script

--- install_dotfiles.bash
+++ install_dotfiles.bash
@@ -1,8 +1,24 @@
 #!/usr/bin/env bash
 
+BIN="$HOME/bin"
+DIR="$(realpath .)"
 
-for FILE in .bashrc .bash_aliases .gitconfig
-do
-    cp ~/$FILE ~/$FILE.old
-    cp ~/code/dotfiles/$FILE ~/$FILE
+if [ ! -d "$BIN" ]; then
+    echo "creating directory: $BIN ..."
+    mkdir "$BIN"
+fi
+
+FILES=(
+    ".bashrc"
+    ".bash_aliases"
+    ".gitconfig"
+    "bin/backup_world.sh"
+)
+for FILE in "${FILES[@]}"; do
+    if [ -f "$HOME/$FILE" ]; then
+        echo "backing up $HOME/$FILE to $HOME/$FILE.old ..."
+        cp "$HOME/$FILE" "$HOME/$FILE.old"
+    fi
+    echo "copying $DIR/$FILE to $HOME/$FILE ..."
+    cp "$DIR/$FILE" "$HOME/$FILE"
 done
