a3ffafd1d4bba12741143db94cb5c3d8960b8e74
Corey Goldberg <1113081+cgoldberg@users.noreply.github.com>
2025-04-30 15:30:36 -0400

    Update image metadata fix script

--- bin/img_metadata_strip_fix.py
+++ bin/img_metadata_strip_fix.py
@@ -1,33 +1,32 @@
 #!/usr/bin/env python3
+# Corey Goldberg, 2014-2025
 #
 # image metadata cleaner
-# Corey Goldberg, 2014, 2019
-#
 
 
 """Recursively scan a directory and clear metadata from image files.
 
-  * removes all metadata (Exif, IPTC, XMP, GPS Info, comment, thumbnail)
-  * works with .jpg and .png formats
-  * modifications are done in-place
+* removes all metadata (Exif, IPTC, XMP, GPS Info, comment, thumbnail)
+* works with .jpg and .png formats
+* modifications are done in-place
 
-  requires:
-    * gexiv2 (GObject-based wrapper for the Exiv2 library
-        - Install on Ubuntu/Debian: $ sudo apt-get install gir1.2-gexiv2-X
-    * exiv2
-        - Install on Ubuntu/Debian: $ sudo apt-get install exiv2
+requires:
+  * gexiv2 (GObject-based wrapper for the Exiv2 library
+      - Install on Ubuntu/Debian: $ sudo apt install gir1.2-gexiv2*
+  * exiv2
+      - Install on Ubuntu/Debian: $ sudo apt install exiv2
 """
 
 
-import subprocess
 import os
+import subprocess
 import sys
 import time
 
 try:
     from gi.repository import GExiv2
 except ImportError:
-    sys.exit('gexiv2 not installed')
+    sys.exit("gexiv2 not installed")
 
 
 def clear_all_metadata(img_path):
@@ -42,14 +41,14 @@ def clear_all_metadata(img_path):
     metadata.save_file()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     dir = os.getcwd()
     for root, dirs, filenames in os.walk(dir):
         for filename in filenames:
             file_path = os.path.join(root, filename)
-            if filename.lower().endswith(('jpg', 'png')):
+            if filename.lower().endswith(("jpg", "png")):
                 # delete all metadata with exiv2
-                subprocess.call(['exiv2', '-d', 'a', 'delete', file_path])
+                subprocess.call(["exiv2", "-d", "a", "delete", file_path])
                 # delete all metadata again with gexiv
                 clear_all_metadata(file_path)
                 print(file_path)
