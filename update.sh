7ab6a9af8db1dac362ca8f9ca9306298e0e088cf
Corey Goldberg <1113081+cgoldberg@users.noreply.github.com>
2025-07-13 19:30:33 -0400

    Refactor update script

--- update.sh
+++ update.sh
@@ -8,8 +8,46 @@ set -e
 
 DOTFILES_HOME="${HOME}/code/dotfiles"
 BIN_DIR="${HOME}/bin"
+CONFIGS=(
+    ".bashrc"
+    ".profile"
+    ".gitconfig"
+)
+LINUX_CONFIGS=(
+    ".bashrc_linux"
+    ".bashrc_linux_selenium"
+)
+WIN_CONFIGS=(
+    ".bashrc_windows"
+    ".gitconfig_win"
+    ".minttyrc"
+)
+SCRIPTS=(
+    "./bin/colors"
+    "./bin/prettyping"
+)
+LINUX_SCRIPTS=(
+    "./bin/now"
+    "./bin/sysinfo"
+)
+GIT_SCRIPTS=(
+    "git-info"
+    "git-score"
+    "git-stat"
+    "git-sync"
+    "git-syncrepo"
+    "git-track-branches"
+)
 
-die() { echo "$*" 1>&2 ; exit 1; }
+die() {
+    tput setaf 1 && echo -en "\u274C " && tput sgr0
+    echo "$*" 1>&2
+    exit 1
+}
+
+if [ ! -d "${DOTFILES_HOME}" ]; then
+    die "fatal: can't find dotfiles repo"
+fi
 
 if [ ! -x "$(command -v git)" ]; then
     die "fatal: can't find git"
@@ -17,10 +55,6 @@ fi
 
 if [ ! -x "$(command -v curl)" ]; then
     die "fatal: can't find curl"
-fi
-
-if [ ! -d "${DOTFILES_HOME}" ]; then
-    die "fatal: can't find dotfiles repo"
 fi
 
 echo "updating local dotfiles and ~/bin scripts from github ..."
@@ -38,41 +72,36 @@ git sync
 git checkout "${default_branch}" >/dev/null 2>&1
 
 echo "copying configs from dotfiles repo master branch to ${HOME}"
-cp .bashrc "${HOME}"
-cp .profile "${HOME}"
-cp .gitconfig "${HOME}"
+for config in "${CONFIGS[@]}"; do
+    cp "${config}" "${HOME}"
+done
 if [[ "${OSTYPE}" == "linux"* ]]; then
     echo "copying linux configs from dotfiles repo master branch to ${HOME}"
-    cp .bashrc_linux "${HOME}"
-    cp .bashrc_linux_selenium "${HOME}"
+    for config in "${LINUX_CONFIGS[@]}"; do
+        cp "${config}" "${HOME}"
+    done
 elif [[ "${OSTYPE}" == "msys" ]]; then
-    echo "copying msys configs from dotfiles repo master branch to ${HOME}"
-    cp .bashrc_windows "${HOME}"
-    cp .gitconfig_win "${HOME}"
-    cp .minttyrc "${HOME}"
+    echo "copying windows configs from dotfiles repo master branch to ${HOME}"
+    for config in "${WIN_CONFIGS[@]}"; do
+        cp "${config}" "${HOME}"
+    done
 fi
 
 echo "copying scripts from dotfiles repo master branch to ${BIN_DIR}"
-cp ./bin/colors "${BIN_DIR}"
-cp ./bin/prettyping "${BIN_DIR}"
+for script in "${SCRIPTS[@]}"; do
+    cp "${script}" "${BIN_DIR}"
+done
 if [[ "${OSTYPE}" == "linux"* ]]; then
     echo "copying linux scripts from dotfiles repo master branch to ${BIN_DIR}"
-    cp ./bin/now "${BIN_DIR}"
-    cp ./bin/sysinfo "${BIN_DIR}"
+    for script in "${SCRIPTS[@]}"; do
+        cp "${script}" "${BIN_DIR}"
+    done
 fi
 
 git checkout "${current_branch}" >/dev/null 2>&1
 
 echo "downloading git scripts from github to ${BIN_DIR}"
-scripts=(
-    "git-info"
-    "git-score"
-    "git-stat"
-    "git-sync"
-    "git-syncrepo"
-    "git-track-branches"
-)
-for script in "${scripts[@]}"; do
+for script in "${GIT_SCRIPTS[@]}"; do
     url="https://raw.githubusercontent.com/cgoldberg/git-scripts/refs/heads/main/${script}"
     curl -fsS --output "${BIN_DIR}/${script}" "${url}"
     chmod +x "${BIN_DIR}/${script}"
