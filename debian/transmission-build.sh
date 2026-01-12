6b41253d2610ab292ef5eae845f1ccde139745ad
Corey Goldberg <1113081+cgoldberg@users.noreply.github.com>
2026-01-12 14:18:17 -0500

    Build transmission from latest tag

--- debian/transmission-build.sh
+++ debian/transmission-build.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 #
-# build/install Transmission with GTK 4 client from source (Debian 13/Trixie)
+# build/install Transmission with GTK 4 client from latest tag in source (Debian 13/Trixie)
 
 set -e
 
 
 sudo apt install -y \
-    cmake libcurl4-openssl-dev libb64-dev libdeflate-dev libevent-dev \
+    clang cmake libcurl4-openssl-dev libb64-dev libdeflate-dev libevent-dev \
     libminiupnpc-dev libnatpmp-dev libsystemd-dev gettext libgtkmm-4.0-dev
 
 export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
@@ -15,7 +15,10 @@ cd ~/code
 git clone --recurse-submodules --shallow-submodules --depth=1 \
     https://github.com/transmission/transmission.git
 cd transmission
+git fetch --tags
+git checkout $(git describe --tags $(git rev-list --tags --max-count=1))
 cmake -B build -DCMAKE_BUILD_TYPE=Release
 cd build
 cmake --build .
 sudo cmake --install .
+git checkout main
